0f1146295f0fcf03ad8374f85cc8ecf37a85d59f
Mario-PinedaD <mariopd2003@hotmail.com>
2024-09-23 08:01:33 -0600

    files

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_backtome
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_backtome/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_backtome
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
